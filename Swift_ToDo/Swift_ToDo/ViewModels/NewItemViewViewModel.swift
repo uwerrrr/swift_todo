658d7205774fb2cb1cc0f53a0b375694f1d5196b
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-11-02 13:44:19 +1100

    add save to firebase

--- Swift_ToDo/Swift_ToDo/ViewModels/NewItemViewViewModel.swift
+++ Swift_ToDo/Swift_ToDo/ViewModels/NewItemViewViewModel.swift
@@ -5,6 +5,8 @@
 //  Created by Van Nguyen on 11/10/2023.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class NewItemViewViewModel: ObservableObject {
@@ -15,7 +17,33 @@ class NewItemViewViewModel: ObservableObject {
     init() {}
     
     func save(){
+        guard canSave else{
+            return
+        }
         
+        // Get current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // Create model
+        let newId = UUID().uuidString
+        let newItem = ToDoListItem(
+            id: newId,
+            title: title,
+            dueDate: dueDate.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isDone: false)
+        
+        
+        // Save model
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("todos")
+            .document("123")
+            .setData(newItem.asDictionary()) // set data as a dictionary
     }
     
     var canSave:Bool {
